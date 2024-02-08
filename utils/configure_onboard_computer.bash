c8a7e589e239d0260da72b76665e410454f8662c
Rafael Perez-Segui <rafa.perez.s98@gmail.com>
2024-02-08 12:00:34 +0100

    Update config to PSDK v3.8

---
+++ utils/configure_onboard_computer.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+# sudo chmod 777 /dev/ttyTHS0
+sudo chmod 777 /dev/ttyACM0
+sudo chmod 777 /dev/ttyUSB0
+
+sudo bash -c "echo 'SUBSYSTEM==\"usb\", ATTRS{id_vendor}==\"2ca3\", MODE=\"0666\"
+SUBSYSTEM==\"usb\", ATTRS{idVendor}==\"2ca3\", MODE=\"0666\"
+' > /etc/udev/rules.d/DJIDevice.rules"
