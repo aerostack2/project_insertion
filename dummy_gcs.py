345434e6a44592cff109a24e4a3f6d93b1747f6e
pariaspe <pedro.ariasp@upm.es>
2025-09-18 13:38:17 +0200

    [fix] added target on missions

--- dummy_gcs.py
+++ dummy_gcs.py
@@ -155,9 +155,11 @@ class DummyGCS(Node):
     def publish_mission(self):
         """Load all three missions to the drone"""
         # scan_mission = AreaScanFollowPathMission(
+        #     target=self.drone_target,
         #     area_corners=[[0.0, 0.0], [10.0, 0.0], [10.0, 10.0], [0.0, 10.0]],
         #     altitude=2.0, speed=1.0)
         scan_mission = AreaScanMission(
+            target=self.drone_target,
             area_corners=[[0.0, 0.0], [10.0, 0.0], [10.0, 10.0], [0.0, 10.0]],
             altitude=2.0, speed=1.0)
 
@@ -168,7 +170,7 @@ class DummyGCS(Node):
         self.uplink.publish(msg)
 
         time.sleep(0.5)
-        take_sample_mission = TakeSampleMission(altitude=1.0, speed=0.5)
+        take_sample_mission = TakeSampleMission(target=self.drone_target, altitude=1.0, speed=0.5)
         msg = MissionUpdate(drone_id=self.drone_target, mission_id=1, action=MissionUpdate.LOAD,
                             mission=take_sample_mission.json())
         self.get_logger().info(f"Publishing Take Sample mission")
@@ -176,7 +178,7 @@ class DummyGCS(Node):
         self.uplink.publish(msg)
 
         time.sleep(0.5)
-        boat_mission = BoatMission(altitude=1.0, speed=0.5)
+        boat_mission = BoatMission(target=self.drone_target, altitude=1.0, speed=0.5)
         msg = MissionUpdate(drone_id=self.drone_target, mission_id=2, action=MissionUpdate.LOAD,
                             mission=boat_mission.json())
         self.get_logger().info(f"Publishing Boat mission")
