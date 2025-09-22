6aa419c9299ff87c8b437df9ac922b46afba9e4e
GPatiA2 <g.glenza@upm.es>
2025-09-22 11:33:38 +0200

    Changed hostname of setup drones to guille

--- utils/setup_m30t.bash
+++ utils/setup_m30t.bash
@@ -3,7 +3,7 @@
 # This script is used to configure the DJI PSDK for the Jetson Orin AGX used in the M300
 
 # Sync date and time with the host
-$HOME/project_insertion/utils/sync_date.bash rafa
+$HOME/project_insertion/utils/sync_date.bash guille 
 
 # Set the USB role to host
 $HOME/project_insertion/utils/config_psdk.bash device
